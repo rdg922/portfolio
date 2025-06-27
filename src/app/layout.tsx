20bd06eb8c53f18c0fbe4df6a73a206d2f7ab442
Rohit Dasgupta <rdg922@gmail.com>
2025-06-27 12:27:30 -0400

    Use collision emoji as temporary favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,9 @@ export const metadata: Metadata = {
   ],
   authors: [{ name: "Rohit Dasgupta" }],
   creator: "Rohit Dasgupta",
+  icons: {
+    icon: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💥</text></svg>",
+  },
   openGraph: {
     title: "ROHIT DASGUPTA | Full Stack Developer & CS Student",
     description:
