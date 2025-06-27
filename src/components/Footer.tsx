343cd224bd1e1f66c4db4297130b5f75593de83a
Rohit Dasgupta <rdg922@gmail.com>
2025-06-27 16:49:59 -0400

    changed with love to be an emoji, update resume

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
 
       <div className="max-w-7xl mx-auto text-center relative z-10">
         <div className="font-mono text-sm text-gray-600 bg-white/80 inline-block px-8 py-4 border-2 border-black shadow-[4px_4px_0px_0px_rgba(0,0,0,1)]">
-          <p>made by rohit dasgupta, with love</p>
+          <p>made with ❤️ by rohit dasgupta</p>
           {/* <p className="mt-2 opacity-60">
             please report any bugs or features here
           </p> */}
